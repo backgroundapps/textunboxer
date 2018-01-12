a3999acc9681ab4c743665f68e2f8edc72faf752
Jonatas Emidio <jonatasemidio@gmail.com>
2018-01-12 09:35:54 -0200

    Update Unboxer.groovy

--- groovy/Unboxer.groovy
+++ groovy/Unboxer.groovy
@@ -1,22 +1,42 @@
-public class Unboxer {
+public class Unboxer
+{
 
   public enum TOKEN { PARENTHESYS, BRACKETS, BRACES }
+
+  private static Unboxer unboxer
+
   private String text
-  private Unboxer unboxer
-  
+  private Point point
+  private TOKEN token
+ 
   private Unboxer() {}
   private Unboxer(String text)
   {
     this.text = text
   }
-  
+
   public static Unboxer on(String text)
   {
-    unboxer = new Unboxer(text)   
-    unboxer
+    unboxer = new Unboxer(text) 
+    return unboxer
   }
 
-  public static Unboxer splitedBy(TOKEN token) {return null}
+  public Unboxer splitedBy(TOKEN token)
+  {
+    unboxer.token = token
+    
+    unboxer
+  }
   
-  public static String run() {return null}
+  public String run()
+  {
+    unboxer.text.replace("(", "\n").replace(")", "\n")
+  }
+
+  class Point
+  {
+    String begin, end
+  }
 }
+
+Unboxer.on("(1 + (1 + 1 - (2+2)))").splitedBy(Unboxer.TOKEN.PARENTHESYS).run()
